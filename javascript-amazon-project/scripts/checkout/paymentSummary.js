bb72a8714f8fe4c201fd8fd170fef9bb5cf8cc08
ConsTantuni <alifaiksayilkan2@hotmail.com>
2024-07-26 03:48:00 +0300

    Create order using the backend

--- javascript-amazon-project/scripts/checkout/paymentSummary.js
+++ javascript-amazon-project/scripts/checkout/paymentSummary.js
@@ -2,6 +2,7 @@ import { cart, updateCartQuantity } from "../../data/cart.js";
 import { getProduct } from "../../data/products.js";
 import { getDeliveryOption } from "../../data/deliveryOptions.js";
 import { formatCurrency } from "../utils/money.js";
+import { addOrder } from "../../data/orders.js";
 
 export function renderPaymentSummary(){
   let productPriceCents = 0;
@@ -59,9 +60,31 @@ export function renderPaymentSummary(){
       </div>
     </div>
 
-    <button class="place-order-button button-primary">
+    <button class="js-place-order-button place-order-button button-primary">
       Place your order
     </button>
   `;
   document.querySelector('.js-payment-summary').innerHTML = paymentSummaryHTML;
+
+  document.querySelector('.js-place-order-button').addEventListener('click', async () => {
+    try{
+      const response = await fetch('https://supersimplebackend.dev/orders', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          cart: cart
+        })
+      });
+  
+      const order = await response.json();
+      addOrder(order);
+
+    } catch(error){
+      console.log('unexpected error. try again later.')
+    }
+
+    window.location.href = 'orders.html';
+  });
 }
